7eeae1c5f4c9ca6b4f1c6f54ddc0e4f84bd2383f
waleed.yk07@gmail.com <waleed.yk07@gmail.com>
2024-02-19 13:28:44 +0500

    backend API url update

--- src/app/service/ivan-gpt-client.service.ts
+++ src/app/service/ivan-gpt-client.service.ts
@@ -18,8 +18,14 @@ export class IvanGptClientService {
 
 	constructor(private httpClient: HttpClient, private messageShareService: MessageShareService) { }
 
+	// Local
 	// private IVAN_GPT_URL = 'http://127.0.0.0:5000/chat';
-	private IVAN_GPT_URL = 'http://ec2-18-234-52-123.compute-1.amazonaws.com:5000/chat';
+	
+	// Production Without LB
+	// private IVAN_GPT_URL = 'http://ec2-18-234-52-123.compute-1.amazonaws.com:5000/chat';
+	
+	// Production With LB
+	private IVAN_GPT_URL = 'http://simosa-backend-env.eba-52rxsarm.us-east-1.elasticbeanstalk.com/chat';
 
 	sendMessage(message: string): Observable<MessageHttpResponse> {
 
